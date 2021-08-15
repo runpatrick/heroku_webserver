650f456946bd818bc94b2588ad6a4ce838978cf3
runpatrick <34929307+runpatrick@users.noreply.github.com>
2021-08-15 01:11:00 -0400

    Update BookmarkServer.py

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -5,39 +5,7 @@ import threading
 from socketserver import ThreadingMixIn
 from urllib.parse import unquote, parse_qs
 
-memory = {}
-
-form = '''
-<!DOCTYPE html>
-<html>
-<head>
-<title>Bookmark Server</title>
-</head>
-<body>
-<form method="POST">
-    <label>Long URI:
-        <input name="longuri">
-    </label>
-    <br>
-    <label>Short name:
-        <input name="shortname">
-    </label>
-    <br>
-    <button type="submit">Save it!</button>
-</form>
-<p>URIs I know about:
-<pre>
-{}
-</pre>
-</body>
-</html>
-'''
-
-class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
-    "This is an HTTPServer that supports thread-based concurrency."
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000)) # Use PORT if it's there.
-    server_address = ('', port)
-    httpd = ThreadHTTPServer(server_address, Shortener)
+def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
